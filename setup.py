70e026ff4fece1044787e76df58a3ca19046e827
ljwolf <levi.john.wolf@gmail.com>
2017-07-13 20:43:44 -0700

    bump version and add maintainer

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup
 
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    from distutils.command.build_py import build_py
+from distutils.command.build_py import build_py
 
-setup(name='esda',  # name of package
-      version='1.0.0dev',
+setup(name='esda', #name of package
+      version='1.0.1dev',
       description='Package with statistics for exploratory spatial data analysis',
-      url='https://github.com/pysal/esda',
-      maintainer='Sergio Rey',
-      maintainer_email='sjsrey@gmail.com',
-      test_suite='nose.collector',
+      url=, 'https://github.com/pysal/esda'
+      maintainer='Sergio Rey', 
+      maintainer_email='sjsrey@gmail.com', 
+      test_suite = 'nose.collector',
       tests_require=['nose'],
-      keywords='spatial statistics',
+      keywords='spatial statistics'
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
@@ -23,14 +20,14 @@ setup(name='esda',  # name of package
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
         ],
       license='3-Clause BSD',
       packages=['esda'],
       install_requires=['numpy', 'scipy', 'libpysal'
-                        ],
+                        ,],
       zip_safe=False,
-      cmdclass={'build.py': build_py})
+      cmdclass = {'build.py':build_py})
