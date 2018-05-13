48b101b5059e208bf62ff9b7543d824a4fa30fa8
Serge Rey <sjsrey@gmail.com>
2018-05-13 15:42:49 +0100

    chore: Update setup for 3+

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(name='esda',  # name of package
       maintainer='Sergio Rey',
       maintainer_email='sjsrey@gmail.com',
       test_suite='nose.collector',
+      py_modules['esda'],
+      python_requires='>3.4',
       tests_require=['nose'],
       keywords='spatial statistics',
       classifiers=[
@@ -20,14 +22,11 @@ setup(name='esda',  # name of package
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
         ],
       license='3-Clause BSD',
       packages=['esda'],
-      install_requires=['numpy', 'scipy', 'libpysal'
-                        ],
+      install_requires=['libpysal'],
       zip_safe=False,
       cmdclass={'build.py': build_py})
