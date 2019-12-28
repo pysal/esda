2121badebd5e7082c5b5ab7101d4e25b21bde27e
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2019-12-29 00:19:03 +0100

    Add remaining tests

--- esda/tests/test_adbscan.py
+++ esda/tests/test_adbscan.py
@@ -8,7 +8,7 @@ class ADBSCAN_Tester(unittest.TestCase):
     def setUp(self):
         np.random.seed(10)
         self.db = pandas.DataFrame(
-            {"X": np.random.random(25), "Y": np.random.random(25)}
+            {"x": np.random.random(25), "y": np.random.random(25)}
         )
         self.lbls = np.array(
             [
@@ -71,9 +71,13 @@ class ADBSCAN_Tester(unittest.TestCase):
         )
 
     def test_adbscan(self):
+        # ------------------------#
+        #           # Single Core #
+        # ------------------------#
         np.random.seed(10)
         ads = adbscan.ADBSCAN(0.03, 3, reps=10, keep_solus=True)
-        _ = ads.fit(self.db)
+        # test there's no labels_, votes, or solus
+        _ = ads.fit(self.db, xy=["x", "y"])
         # Params
         self.assertAlmostEqual(ads.eps, 0.03)
         self.assertEqual(ads.min_samples, 3)
@@ -97,15 +101,90 @@ class ADBSCAN_Tester(unittest.TestCase):
             [4, 8, 4, 5, 6, 6, 8, 5, 6, 6, 3, 2, 7, 6, 5, 4, 5, 6, 5, 4, 6, 6, 7, 3, 6]
         )
         np.testing.assert_equal(ads.solus.astype(int).sum(axis=1).values, i_sum)
+        # ------------------------#
+        #           # Multi Core #
+        # ------------------------#
+        np.random.seed(10)
+        ads = adbscan.ADBSCAN(0.03, 3, reps=10, keep_solus=True, n_jobs=-1)
+        _ = ads.fit(self.db, xy=["x", "y"])
+        # Params
+        self.assertEqual(ads.n_jobs, -1)
+        # Labels
+        np.testing.assert_equal(ads.labels_, self.lbls)
+        # Votes
+        votes = pandas.DataFrame({"lbls": self.lbls, "pct": self.pcts})
+        np.testing.assert_equal(ads.votes["lbls"].values, self.lbls)
+        np.testing.assert_almost_equal(ads.votes["pct"].values, votes["pct"].values)
+        # Solus (only testing the sums as there're too many values)
+        np.testing.assert_equal(ads.solus.astype(int).sum().sum(), 133)
+        rep_sum = np.array([9, 24, 16, 13, 9, 16, 7, 13, 14, 12])
+        np.testing.assert_equal(ads.solus.astype(int).sum().values, rep_sum)
+        i_sum = np.array(
+            [4, 8, 4, 5, 6, 6, 8, 5, 6, 6, 3, 2, 7, 6, 5, 4, 5, 6, 5, 4, 6, 6, 7, 3, 6]
+        )
+        np.testing.assert_equal(ads.solus.astype(int).sum(axis=1).values, i_sum)
 
 
 class Remap_lbls_Tester(unittest.TestCase):
-    pass
-
+    def setUp(self):
+        self.db = pandas.DataFrame({"X": [0, 0.1, 4, 6, 5], \
+                               "Y": [0, 0.2, 5, 7, 5] \
+                              })
+        self.solus = pandas.DataFrame({"rep-00": [0, 0, 7, 7, -1], \
+                                  "rep-01": [4, 4, -1, 6, 6], \
+                                  "rep-02": [5, 5, 8, 8, 8] \
+                                 })
+    def test_remap_lbls(self):
+        vals = np.array([[0, 0, 0], [0, 0, 0], [7, -1, 7], [7, 7, 7], [-1, 7, 7]])
+        # ------------------------#
+        #           # Single Core #
+        # ------------------------#
+        lbls = adbscan.remap_lbls(self.solus, self.db)
+        # Column names
+        np.testing.assert_equal(self.solus.columns.values, lbls.columns.values)
+        # Values
+        np.testing.assert_equal(lbls.values, vals)
+        # ------------------------#
+        #            # Multi Core #
+        # ------------------------#
+        lbls = adbscan.remap_lbls(self.solus, self.db, n_jobs=-1)
+        # Column names
+        np.testing.assert_equal(self.solus.columns.values, lbls.columns.values)
+        # Values
+        np.testing.assert_equal(lbls.values, vals)
+ 
 
 class Ensemble_Tester(unittest.TestCase):
-    pass
-
+    def setUp(self):
+        self.db = pandas.DataFrame({"X": [0, 0.1, 4, 6, 5], \
+                               "Y": [0, 0.2, 5, 7, 5] \
+                              })\
+                              .rename(lambda i: 'i_'+str(i))
+        self.solus = pandas.DataFrame({"rep-00": [0, 0, 7, 7, -1], \
+                                  "rep-01": [4, 4, -1, 6, 6], \
+                                  "rep-02": [5, 5, 8, 8, 8] \
+                                 })\
+                              .rename(lambda i: 'i_'+str(i))
+    def test_ensemble(self):
+        vals = np.array([[0.0, 1.0], [0.0, 1.0], [7.0, 0.6666666666666666], [7.0, 1.0], [7.0, 0.6666666666666666]])
+        # ------------------------#
+        #           # Single Core #
+        # ------------------------#
+        ensemble_solu = adbscan.ensemble(self.solus, self.db)
+        # Column names
+        np.testing.assert_equal(ensemble_solu.columns.values.tolist(), ['lbls', 'pct'])
+        # Index
+        # Values
+        np.testing.assert_almost_equal(ensemble_solu.values, vals)
+        # ------------------------#
+        #           # Multi  Core #
+        # ------------------------#
+        ensemble_solu = adbscan.ensemble(self.solus, self.db, n_jobs=-1)
+        # Column names
+        np.testing.assert_equal(ensemble_solu.columns.values.tolist(), ['lbls', 'pct'])
+        # Values
+        np.testing.assert_almost_equal(ensemble_solu.values, vals)
+  
 
 suite = unittest.TestSuite()
 test_classes = [ADBSCAN_Tester, Remap_lbls_Tester, Ensemble_Tester]
