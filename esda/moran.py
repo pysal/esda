d0c91c29799b3805f2ad53c24983b64c701b7b17
ljwolf <levi.john.wolf@gmail.com>
2020-04-20 12:35:57 +0100

    Rewrite __crand_lite into __crand and homogenize
    This keeps the conditional randomization in a single place and
    ensures that the objects are all the same, regardless of input
    parameters.

--- esda/moran.py
+++ esda/moran.py
@@ -847,11 +847,14 @@ class Moran_Local(object):
     moved into unittests that are conditional on architectures
     """
     def __init__(self, y, w, transformation="r", permutations=PERMUTATIONS,
-                 geoda_quads=False):
+                 keep_simulations=None, geoda_quads=False):
         y = np.asarray(y).flatten()
         self.y = y
         n = len(y)
         self.n = n
+        if keep_simulations is None:
+            # if the user doesn't set it, use a default that depends on size
+            keep_simulations = n <= LARGE
         self.n_1 = n - 1
         z = y - y.mean()
         # setting for floating point noise
@@ -872,60 +875,35 @@ class Moran_Local(object):
             quads = [1, 3, 2, 4]
         self.quads = quads
         self.__quads()
-        if permutations and n < LARGE:
-            self.__crand()
-            sim = np.transpose(self.rlisas)  
-            above = sim >= self.Is
-            larger = above.sum(0)
+        if permutations:
+            if n > LARGE and keep_simulations:
+                warn(f'Permutation inference on datasets larger than {LARGE} observations'
+                      ' may be incredibly slow and memory-intensive, or both. Performance' 
+                      ' may suffer. We strongly recommend using `keep_simulations=False`'
+                      ' in this case to avoid significant memory pressure.')
+            larger = self.__crand(keep_simulations)
             low_extreme = (self.permutations - larger) < larger
             larger[low_extreme] = self.permutations - larger[low_extreme]
             self.p_sim = (larger + 1.0) / (permutations + 1.0)
-            self.sim = sim
-            self.EI_sim = sim.mean(axis=0)
-            self.seI_sim = sim.std(axis=0)
-            self.VI_sim = self.seI_sim * self.seI_sim
-            self.z_sim = (self.Is - self.EI_sim) / self.seI_sim
-            self.p_z_sim = 1 - stats.norm.cdf(np.abs(self.z_sim))
-        elif permutations and n >= LARGE:
-            print('Large N')
-            larger = self.__crand_lite()
-            low_extreme = (self.permutations - larger) < larger
-            larger[low_extreme] = self.permutations - larger[low_extreme]
-            self.p_sim = (larger + 1.0) / (permutations + 1.0)
+            if keep_simulations:
+                self.sim = self.rlisas
+                self.EI_sim = sim.mean(axis=0)
+                self.seI_sim = sim.std(axis=0)
+                self.VI_sim = self.seI_sim * self.seI_sim
+                self.z_sim = (self.Is - self.EI_sim) / self.seI_sim
+                self.p_z_sim = 1 - stats.norm.cdf(np.abs(self.z_sim))
+            else:
+                # preserve attributes to ensure parity
+                self.sim = self.rlisas = None
+                self.EI_sim = self.seI_sim = self.VI_sim = np.nan
+                self.z_sim = self.p_z_sim = np.nan
+            
 
     def calc(self, w, z):
         zl = slag(w, z)
         return self.n_1 * self.z * zl / self.den
 
-    def __crand_lite(self):
-        """
-        do not store all of the simulated lisas
-        accumulate counts
-        returns an array with the number of times the observed values are exceeded
-        """
-        larger = np.zeros((self.n))
-        z = self.z
-        n_1 = self.n - 1
-        prange = list(range(self.permutations))
-        k = self.w.max_neighbors + 1
-        nn = self.n - 1
-        rids = np.array([np.random.permutation(nn)[0:k] for i in prange])
-        ids = np.arange(self.w.n)
-        ido = self.w.id_order
-        w = [self.w.weights[ido[i]] for i in ids]
-        wc = [self.w.cardinalities[ido[i]] for i in ids]
-
-        for i in range(self.w.n):
-            idsi = ids[ids != i]
-            np.random.shuffle(idsi)
-            tmp = z[idsi[rids[:, 0:wc[i]]]]
-            lisas_i = z[i] * (w[i] * tmp).sum(1)
-            larger[i] = (lisas_i > self.Is[i]).sum()
-
-        return larger
-
-
-    def __crand(self):
+    def __crand(self, keep_simulations):
         """
         conditional randomization
 
@@ -939,7 +917,9 @@ class Moran_Local(object):
 
         """
         z = self.z
-        lisas = np.zeros((self.n, self.permutations))  # this is memory bound
+        if keep_simulations:
+            lisas = np.empty((self.n, self.permutations))  # this is memory bound
+        larger = np.empty((self.n))
         n_1 = self.n - 1
         prange = list(range(self.permutations))
         k = self.w.max_neighbors + 1
@@ -949,13 +929,18 @@ class Moran_Local(object):
         ido = self.w.id_order
         w = [self.w.weights[ido[i]] for i in ids]
         wc = [self.w.cardinalities[ido[i]] for i in ids]
-
-        for i in range(self.w.n):
+        empirical_Is = self.Is
+        
+        for i,lmo in enumerate(empirical_Is):
             idsi = ids[ids != i]
             np.random.shuffle(idsi)
             tmp = z[idsi[rids[:, 0:wc[i]]]]
-            lisas[i] = z[i] * (w[i] * tmp).sum(1)
+            lisas_i = z[i] * (w[i] * tmp).sum(axis=1)
+            if keep_simulations:
+                lisas[i] = lisas_i
+            larger[i] = (lisas_i > lmo).sum()
         self.rlisas = (n_1 / self.den) * lisas
+        return larger
 
     def __quads(self):
         zl = slag(self.w, self.z)
