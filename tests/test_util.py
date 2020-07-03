1caa226c93450865bb0b4d7b2d59d9297c411b1f
ljwolf <levi.john.wolf@gmail.com>
2020-07-03 22:50:07 +0100

    fix false discovery rate filtering

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,18 +4,18 @@ from esda import util
 from esda import moran
 import numpy as np
 
+
 class Fdr_Tester(unittest.TestCase):
     def setUp(self):
         self.w = libpysal.io.open(libpysal.examples.get_path("stl.gal")).read()
         f = libpysal.io.open(libpysal.examples.get_path("stl_hom.txt"))
-        self.y = np.array(f.by_col['HR8893'])
+        self.y = np.array(f.by_col["HR8893"])
 
     def test_fdr(self):
-        np.random.seed(10)
-        lm = moran.Moran_Local(self.y, self.w, transformation="r",
-                               permutations=999)
-        self.assertAlmostEqual(util.fdr(lm.p_sim, 0.1),
-                               0.002564102564102564)
+        lm = moran.Moran_Local(
+            self.y, self.w, transformation="r", permutations=999, seed=10
+        )
+        self.assertAlmostEqual(util.fdr(lm.p_sim, 0.1), 0.002564102564102564)
         self.assertAlmostEqual(util.fdr(lm.p_sim, 0.05), 0.001282051282051282)
 
 
@@ -25,6 +25,6 @@ for i in test_classes:
     a = unittest.TestLoader().loadTestsFromTestCase(i)
     suite.addTest(a)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     runner.run(suite)
