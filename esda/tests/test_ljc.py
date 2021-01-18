75318b4dc72e4d6bf83ef87a4c27420d33119cf3
ljwolf <levi.john.wolf@gmail.com>
2021-01-18 16:01:34 +0000

    update imports for tests, too

--- esda/tests/test_ljc.py
+++ esda/tests/test_ljc.py
@@ -4,27 +4,27 @@ import numpy as np
 from libpysal.weights.util import lat2W
 from libpysal.common import pandas
 
-from esda.join_count_local import Join_Count_Local
+from esda.join_counts_local import Join_Counts_Local
 
 PANDAS_EXTINCT = pandas is None
 
-class Join_Count_Locals_Tester(unittest.TestCase):
+class Join_Counts_Locals_Tester(unittest.TestCase):
     """Unit test for Local Join Counts (univariate)"""
     def setUp(self):
         self.w = lat2W(4, 4)
         self.y = np.ones(16)
         self.y[0:8] = 0
 
-    def test_Join_Count_Locals(self):
+    def test_Join_Counts_Locals(self):
             """Test method"""
             np.random.seed(12345)
-            ljc = Join_Count_Local(connectivity=self.w).fit(self.y)
+            ljc = Join_Counts_Local(connectivity=self.w).fit(self.y)
             assert np.array_equal(ljc.LJC, [0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 2, 2, 3, 3, 2])
             
             
 suite = unittest.TestSuite()
 test_classes = [
-    Join_Count_Locals_Tester
+    Join_Counts_Locals_Tester
 ]
 for i in test_classes:
     a = unittest.TestLoader().loadTestsFromTestCase(i)
