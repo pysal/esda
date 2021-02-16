796c6e0110866efc4cbe2bd2e11b70c38cfb5a85
Serge Rey <sjsrey@gmail.com>
2021-02-16 08:32:29 -0800

    Add tests for crand and parallel implementation

--- esda/tests/test_moran.py
+++ esda/tests/test_moran.py
@@ -118,6 +118,20 @@ class Moran_Local_Tester(unittest.TestCase):
         self.assertAlmostEqual(lm.z_sim[0], -0.6990291160835514)
         self.assertAlmostEqual(lm.p_z_sim[0], 0.24226691753791396)
 
+    def test_Moran_Local_parallel(self):
+        lm = moran.Moran_Local(
+            self.y,
+            self.w,
+            transformation="r",
+            n_jobs=-1,
+            permutations=99,
+            keep_simulations=True,
+            seed=SEED,
+        )
+        self.assertAlmostEqual(lm.z_sim[0], -0.6990291160835514)
+        self.assertAlmostEqual(lm.p_z_sim[0], 0.24226691753791396)
+
+
     @unittest.skip("This function is being deprecated in the next release.")
     def test_by_col(self):
         import pandas as pd
