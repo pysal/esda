7b9e98bbb37397756be3d33e24e8885da844eb02
eli knaap <ek@knaaptime.com>
2023-07-28 12:55:23 -0700

    scm init

--- esda/__init__.py
+++ esda/__init__.py
@@ -3,6 +3,10 @@
 =================================================
 
 """
+
+import contextlib
+from importlib.metadata import PackageNotFoundError, version
+
 from . import _version, adbscan, shape  # noqa F401
 from .gamma import Gamma  # noqa F401
 from .geary import Geary  # noqa F401
@@ -15,13 +19,26 @@ from .join_counts_local_bv import Join_Counts_Local_BV  # noqa F401
 from .join_counts_local_mv import Join_Counts_Local_MV  # noqa F401
 from .lee import Spatial_Pearson, Spatial_Pearson_Local  # noqa F401
 from .losh import LOSH  # noqa F401
-from .map_comparison import (areal_entropy, completeness,  # noqa F401
-                             external_entropy, homogeneity, overlay_entropy)
-from .moran import (Moran, Moran_BV, Moran_BV_matrix, Moran_Local,  # noqa F401
-                    Moran_Local_BV, Moran_Local_Rate, Moran_Rate)
+from .map_comparison import (
+    areal_entropy,
+    completeness,  # noqa F401
+    external_entropy,
+    homogeneity,
+    overlay_entropy,
+)
+from .moran import (
+    Moran,
+    Moran_BV,
+    Moran_BV_matrix,
+    Moran_Local,  # noqa F401
+    Moran_Local_BV,
+    Moran_Local_Rate,
+    Moran_Rate,
+)
 from .silhouettes import boundary_silhouette, path_silhouette  # noqa F401
 from .smaup import Smaup  # noqa F401
 from .topo import isolation, prominence  # noqa F401
 from .util import fdr  # noqa F401
 
-__version__ = _version.get_versions()["version"]
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = version("esda")
