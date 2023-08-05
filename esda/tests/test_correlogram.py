94c6b568c126d69ae54ebdaf08aabfba36209bf5
eli knaap <ek@knaaptime.com>
2023-08-05 15:42:17 -0700

    knn test

--- esda/tests/test_correlogram.py
+++ esda/tests/test_correlogram.py
@@ -10,13 +10,27 @@ sac = gpd.read_file(examples.load_example("Sacramento1").get_path("sacramentot2.
 sac = sac.to_crs(sac.estimate_utm_crs())  # now in meters)
 
 distances = [i + 500 for i in range(0, 2000, 500)]
+kdists = list(range(1, 6))
 
-def test_correlogram():
+
+def test_distance_correlogram():
 
     corr = correlogram(sac, "HH_INC", distances)
 
     test_data = np.array(
-        [0.05822723177762817, 0.49206877942505206, 0.45494217612839183, 0.5625914469490942]
+        [
+            0.05822723177762817,
+            0.49206877942505206,
+            0.45494217612839183,
+            0.5625914469490942,
+        ]
     )
 
+    assert_array_almost_equal(corr.I, test_data)
+
+
+def test_k_distance_correlogram():
+    corr = correlogram(sac, "HH_INC", kdists, distance_type="knn")
+
+    test_data = np.array([0.62411734, 0.59734846, 0.56958116, 0.54252517, 0.54093269])
     assert_array_almost_equal(corr.I, test_data)
