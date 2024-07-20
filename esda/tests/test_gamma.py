6c34d273ad9b9a5d6df4e328b97e45a9b17e5f03
Martin Fleischmann <martin@martinfleischmann.net>
2024-07-20 15:31:03 +0200

    ENH: natively support Graph in getisord and gamma modules (#326)
    * ENH: natively support Graph in getisord module
    
    * ENH: natively support Graph in gamma module
    
    * use index_pairs
    
    * skpif
    
    * summarize
    
    * temporarily revert lib pin
    
    * pin back to 4.12

--- esda/tests/test_gamma.py
+++ esda/tests/test_gamma.py
@@ -1,93 +1,106 @@
-import unittest
+import pytest
 
 import numpy as np
-from libpysal.common import pandas
-from libpysal.weights.util import lat2W
+import libpysal
 
 from ..gamma import Gamma
 
-PANDAS_EXTINCT = pandas is None
+
+parametrize_lat = pytest.mark.parametrize(
+    "w",
+    [
+        libpysal.weights.util.lat2W(4, 4),
+        libpysal.graph.Graph.from_W(libpysal.weights.util.lat2W(4, 4)),
+    ],
+    ids=["W", "Graph"],
+)
 
 
-class Gamma_Tester(unittest.TestCase):
+class TestGamma:
     """Unit test for Gamma Index"""
 
-    def setUp(self):
-        self.w = lat2W(4, 4)
+    def setup_method(self):
         self.y = np.ones(16)
         self.y[0:8] = 0
         np.random.seed(12345)
-        self.g = Gamma(self.y, self.w)
 
-    def test_Gamma(self):
+    @parametrize_lat
+    def test_Gamma(self, w):
         """Test method"""
-        g = self.g
-        self.assertAlmostEqual(g.g, 20.0)
-        self.assertAlmostEqual(g.g_z, 3.1879280354548638)
-        self.assertAlmostEqual(g.p_sim_g, 0.0030000000000000001)
-        self.assertAlmostEqual(g.min_g, 0.0)
-        self.assertAlmostEqual(g.max_g, 20.0)
-        self.assertAlmostEqual(g.mean_g, 11.093093093093094)
+        g = Gamma(self.y, w)
+
+        np.testing.assert_allclose(g.g, 20.0)
+        np.testing.assert_allclose(g.g_z, 3.1879280354548638)
+        np.testing.assert_allclose(g.p_sim_g, 0.0030000000000000001)
+        np.testing.assert_allclose(g.min_g, 0.0)
+        np.testing.assert_allclose(g.max_g, 20.0)
+        np.testing.assert_allclose(g.mean_g, 11.093093093093094)
+
+    @parametrize_lat
+    def test_Gamma_s(self, w):
         np.random.seed(12345)
-        g1 = Gamma(self.y, self.w, operation="s")
-        self.assertAlmostEqual(g1.g, 8.0)
-        self.assertAlmostEqual(g1.g_z, -3.7057554345954791)
-        self.assertAlmostEqual(g1.p_sim_g, 0.001)
-        self.assertAlmostEqual(g1.min_g, 14.0)
-        self.assertAlmostEqual(g1.max_g, 48.0)
-        self.assertAlmostEqual(g1.mean_g, 25.623623623623622)
+        g1 = Gamma(self.y, w, operation="s")
+        np.testing.assert_allclose(g1.g, 8.0)
+        np.testing.assert_allclose(g1.g_z, -3.7057554345954791)
+        np.testing.assert_allclose(g1.p_sim_g, 0.001)
+        np.testing.assert_allclose(g1.min_g, 14.0)
+        np.testing.assert_allclose(g1.max_g, 48.0)
+        np.testing.assert_allclose(g1.mean_g, 25.623623623623622)
+
+    @parametrize_lat
+    def test_Gamma_a(self, w):
         np.random.seed(12345)
-        g2 = Gamma(self.y, self.w, operation="a")
-        self.assertAlmostEqual(g2.g, 8.0)
-        self.assertAlmostEqual(g2.g_z, -3.7057554345954791)
-        self.assertAlmostEqual(g2.p_sim_g, 0.001)
-        self.assertAlmostEqual(g2.min_g, 14.0)
-        self.assertAlmostEqual(g2.max_g, 48.0)
-        self.assertAlmostEqual(g2.mean_g, 25.623623623623622)
+        g2 = Gamma(self.y, w, operation="a")
+        np.testing.assert_allclose(g2.g, 8.0)
+        np.testing.assert_allclose(g2.g_z, -3.7057554345954791)
+        np.testing.assert_allclose(g2.p_sim_g, 0.001)
+        np.testing.assert_allclose(g2.min_g, 14.0)
+        np.testing.assert_allclose(g2.max_g, 48.0)
+        np.testing.assert_allclose(g2.mean_g, 25.623623623623622)
+
+    @parametrize_lat
+    def test_Gamma_standardize(self, w):
         np.random.seed(12345)
-        g3 = Gamma(self.y, self.w, standardize=True)
-        self.assertAlmostEqual(g3.g, 32.0)
-        self.assertAlmostEqual(g3.g_z, 3.7057554345954791)
-        self.assertAlmostEqual(g3.p_sim_g, 0.001)
-        self.assertAlmostEqual(g3.min_g, -48.0)
-        self.assertAlmostEqual(g3.max_g, 20.0)
-        self.assertAlmostEqual(g3.mean_g, -3.2472472472472473)
+        g3 = Gamma(self.y, w, standardize=True)
+        np.testing.assert_allclose(g3.g, 32.0)
+        np.testing.assert_allclose(g3.g_z, 3.7057554345954791)
+        np.testing.assert_allclose(g3.p_sim_g, 0.001)
+        np.testing.assert_allclose(g3.min_g, -48.0)
+        np.testing.assert_allclose(g3.max_g, 20.0)
+        np.testing.assert_allclose(g3.mean_g, -3.2472472472472473)
+
+    @parametrize_lat
+    def test_Gamma_op(self, w):
         np.random.seed(12345)
+        if isinstance(w, libpysal.graph.Graph):
+            pytest.skip("Calleble not supported with Graph")
 
         def func(z, i, j):
             q = z[i] * z[j]
             return q
 
-        g4 = Gamma(self.y, self.w, operation=func)
-        self.assertAlmostEqual(g4.g, 20.0)
-        self.assertAlmostEqual(g4.g_z, 3.1879280354548638)
-        self.assertAlmostEqual(g4.p_sim_g, 0.0030000000000000001)
+        g4 = Gamma(self.y, w, operation=func)
+        np.testing.assert_allclose(g4.g, 20.0)
+        np.testing.assert_allclose(g4.g_z, 3.1879280354548638)
+        np.testing.assert_allclose(g4.p_sim_g, 0.0030000000000000001)
 
-    @unittest.skipIf(PANDAS_EXTINCT, "missing pandas")
-    def test_by_col(self):
+    @parametrize_lat
+    def test_by_col(self, w):
         import pandas as pd
 
+        g = Gamma(self.y, w)
+
         df = pd.DataFrame(self.y, columns=["y"])
-        r1 = Gamma.by_col(df, ["y"], w=self.w)
-        self.assertIn("y_gamma", r1.columns)
-        self.assertIn("y_p_sim", r1.columns)
+        r1 = Gamma.by_col(df, ["y"], w=w)
+        assert "y_gamma" in r1.columns
+        assert "y_p_sim" in r1.columns
         this_gamma = np.unique(r1.y_gamma.values)
         this_pval = np.unique(r1.y_p_sim.values)
-        np.testing.assert_allclose(this_gamma, self.g.g)
-        np.testing.assert_allclose(this_pval, self.g.p_sim)
-        Gamma.by_col(df, ["y"], inplace=True, operation="s", w=self.w)
+
+        np.testing.assert_allclose(this_gamma, g.g)
+        np.testing.assert_allclose(this_pval, g.p_sim)
+        Gamma.by_col(df, ["y"], inplace=True, operation="s", w=w)
         this_gamma = np.unique(df.y_gamma.values)
         this_pval = np.unique(df.y_p_sim.values)
         np.testing.assert_allclose(this_gamma, 8.0)
         np.testing.assert_allclose(this_pval, 0.001)
-
-
-suite = unittest.TestSuite()
-test_classes = [Gamma_Tester]
-for i in test_classes:
-    a = unittest.TestLoader().loadTestsFromTestCase(i)
-    suite.addTest(a)
-
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
--- esda/tests/test_getisord.py
+++ esda/tests/test_getisord.py
@@ -1,90 +1,86 @@
-import pytest
-import unittest
-
+import libpysal
 import numpy as np
-from libpysal.common import ATOL, RTOL, pandas
+import pytest
+from libpysal.common import ATOL, RTOL
 from libpysal.weights.distance import DistanceBand
 
 from .. import getisord
 
 POINTS = np.array([(10, 10), (20, 10), (40, 10), (15, 20), (30, 20), (30, 30)])
-W = DistanceBand(POINTS, threshold=15)
-Y = np.array([2, 3, 3.2, 5, 8, 7])
 
-PANDAS_EXTINCT = pandas is None
+parametrize_w = pytest.mark.parametrize(
+    "w",
+    [
+        DistanceBand(POINTS, threshold=15),
+        libpysal.graph.Graph.from_W(DistanceBand(POINTS, threshold=15)),
+    ],
+    ids=["W", "Graph"],
+)
 
 
-class G_Tester(unittest.TestCase):
-    def setUp(self):
-        self.w = W
-        self.y = Y
+class TestGetisG:
+    def setup_method(self):
+        self.y = np.array([2, 3, 3.2, 5, 8, 7])
         np.random.seed(10)
 
-    def test_G(self):
-        g = getisord.G(self.y, self.w)
+    @parametrize_w
+    def test_G(self, w):
+        g = getisord.G(self.y, w)
         np.testing.assert_allclose(g.G, 0.55709779, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(g.p_norm, 0.172936, rtol=RTOL, atol=ATOL)
 
-    @unittest.skipIf(PANDAS_EXTINCT, "missing pandas")
-    def test_by_col(self):
+    @parametrize_w
+    def test_by_col(self, w):
         import pandas as pd
 
         df = pd.DataFrame(self.y, columns=["y"])
         np.random.seed(12345)
-        r1 = getisord.G.by_col(df, ["y"], w=self.w)
+        r1 = getisord.G.by_col(df, ["y"], w=w)
         this_getisord = np.unique(r1.y_g.values)
         this_pval = np.unique(r1.y_p_sim.values)
         np.random.seed(12345)
-        stat = getisord.G(self.y, self.w)
+        stat = getisord.G(self.y, w)
         np.testing.assert_allclose(this_getisord, stat._statistic)
         np.testing.assert_allclose(this_pval, stat.p_sim)
 
 
-class G_Local_Tester(unittest.TestCase):
-    def setUp(self):
-        self.w = W
-        self.y = Y
+class TestGLocal:
+    def setup_method(self):
+        self.y = np.array([2, 3, 3.2, 5, 8, 7])
         np.random.seed(10)
 
-    def test_G_Local_Binary(self):
-        lg = getisord.G_Local(self.y, self.w, transform="B", seed=10)
+    @parametrize_w
+    def test_G_Local_Binary(self, w):
+        lg = getisord.G_Local(self.y, w, transform="B", seed=10)
         np.testing.assert_allclose(lg.Zs[0], -1.0136729, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(lg.p_sim[0], 0.102, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(lg.p_z_sim[0], 0.153923, rtol=RTOL, atol=ATOL)
 
-    def test_G_Local_Row_Standardized(self):
-        lg = getisord.G_Local(self.y, self.w, transform="R", seed=10)
+    @parametrize_w
+    def test_G_Local_Row_Standardized(self, w):
+        lg = getisord.G_Local(self.y, w, transform="R", seed=10)
         np.testing.assert_allclose(lg.Zs[0], -0.62074534, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(lg.p_sim[0], 0.102, rtol=RTOL, atol=ATOL)
 
-    def test_G_star_Local_Binary(self):
-        lg = getisord.G_Local(self.y, self.w, transform="B", star=True, seed=10)
+    @parametrize_w
+    def test_G_star_Local_Binary(self, w):
+        lg = getisord.G_Local(self.y, w, transform="B", star=True, seed=10)
         np.testing.assert_allclose(lg.Zs[0], -1.39727626, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(lg.p_sim[0], 0.102, rtol=RTOL, atol=ATOL)
 
-    def test_G_star_Row_Standardized(self):
+    @parametrize_w
+    def test_G_star_Row_Standardized(self, w):
         with pytest.warns(UserWarning, match="Gi\\* requested, but"):
-            lg = getisord.G_Local(self.y, self.w, transform="R", star=True, seed=10)
+            lg = getisord.G_Local(self.y, w, transform="R", star=True, seed=10)
         np.testing.assert_allclose(lg.Zs[0], -0.62488094, rtol=RTOL, atol=ATOL)
         np.testing.assert_allclose(lg.p_sim[0], 0.102, rtol=RTOL, atol=ATOL)
 
-    @unittest.skipIf(PANDAS_EXTINCT, "missing pandas")
-    def test_by_col(self):
+    @parametrize_w
+    def test_by_col(self, w):
         import pandas as pd
 
         df = pd.DataFrame(self.y, columns=["y"])
-        r1 = getisord.G_Local.by_col(df, ["y"], w=self.w, seed=12345)
-        stat = getisord.G_Local(self.y, self.w, seed=12345)
+        r1 = getisord.G_Local.by_col(df, ["y"], w=w, seed=12345)
+        stat = getisord.G_Local(self.y, w, seed=12345)
         np.testing.assert_allclose(r1.y_g_local.values, stat.Gs)
         np.testing.assert_allclose(r1.y_p_sim, stat.p_sim)
-
-
-suite = unittest.TestSuite()
-test_classes = [G_Tester, G_Local_Tester]
-for i in test_classes:
-    a = unittest.TestLoader().loadTestsFromTestCase(i)
-    suite.addTest(a)
-
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
