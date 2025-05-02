7a2dcdf712db6d69cbf89217a6d72a4ccab18c03
Levi John Wolf <levi.john.wolf@gmail.com>
2025-05-02 21:21:12 +0100

    update significance testing tests

--- esda/tests/test_significance.py
+++ esda/tests/test_significance.py
@@ -1,67 +1,60 @@
 import numpy
 import esda
-import pandas
+from esda.significance import calculate_significance
+import pytest
 from libpysal.weights import Voronoi
 
-#this was more a validation exercise than a test. 
-#Set tests to check:
-#1. the results of the two sided are always greater than the directed
-#2. the results of the directed are equal to either the lesser or greater
-#3. the folded variant is close to the two-sided variant in a normal problem, 
-#   but is similar to the one-sided test in a very skewed problem
-#4. all p-values are between 0 and 1, with some p-values near 1
-#5. no directed p-value will be bigger than .5
-
-def test_significance():
-    raise NotImplementedError()
-
-"""
-coordinates = numpy.random.random(size=(2000, 2))
-x = numpy.random.normal(size=(2000,))
-w = Voronoi(coordinates, clip="bbox")
+numpy.random.seed(2478879)
+coordinates = numpy.random.random(size=(800, 2))
+x = numpy.random.normal(size=(800,))
+w = Voronoi(coordinates, clip="bounding_box")
 w.transform = "r"
-stat = esda.Moran_Local(x, w)
+stat = esda.Moran_Local(x, w, permutations=19)
 
-ts = calculate_significance(stat.Is, stat.rlisas, method="two-sided")
-di = calculate_significance(stat.Is, stat.rlisas, method="directed")
-lt = calculate_significance(stat.Is, stat.rlisas, method="lesser")
-gt = calculate_significance(stat.Is, stat.rlisas, method="greater")
-fo = calculate_significance(stat.Is, stat.rlisas, method="folded")
+@pytest.mark.parametrize("alternative", ["two-sided", "directed", "lesser", "greater", "folded"])
+def test_execution_and_range(alternative):
+    out = calculate_significance(stat.Is, stat.rlisas, alternative=alternative)
+    assert (out > 0).all() & (out <= 1).all(), f'p-value out of bounds for method {alternative}'
+    if alternative == 'directed':
+        assert out.max() <= .5, f"max p-value is too large for method {alternative}"
+    else:
+        assert out.max() >= .5, f"max p-value is too small for method {alternative}"
+    
 
-numpy.testing.assert_array_equal(
-    numpy.minimum(lt, gt), di
-)  # di is just the minimum of the two tests
+def test_alternative_relationships():
+    two_sided = calculate_significance(
+        stat.Is, 
+        stat.rlisas, 
+        alternative="two-sided"
+    )
+    directed = calculate_significance(
+        stat.Is, 
+        stat.rlisas, 
+        alternative="directed"
+    )
+    lesser = calculate_significance(
+        stat.Is, 
+        stat.rlisas, 
+        alternative="lesser"
+    )
+    greater = calculate_significance(
+        stat.Is, 
+        stat.rlisas, 
+        alternative="greater"
+    )
+    folded = calculate_significance(
+        stat.Is, 
+        stat.rlisas, 
+        alternative="folded"
+    )
 
-print(
-    f"directed * 2 is the same as two-sided {(di*2 == ts).mean()*100}% of the time"
-)
-
-print(
-    pandas.DataFrame(
-        numpy.column_stack((ts, di, fo, lt, gt)),
-        columns=["two-sided", "directed", "folded", "lt", "gt"],
-    ).corr()
-)
-
-answer = input("run big simulation? [y/n]")
-if answer.lower().startswith("y"):
-    all_correlations = []
-    for i in range(1000):
-        x = numpy.random.normal(size=(2000,))
-        stat = esda.Moran_Local(x, w)
-        ts = calculate_significance(stat.Is, stat.rlisas, method="two-sided")
-        di = calculate_significance(stat.Is, stat.rlisas, method="directed")
-        lt = calculate_significance(stat.Is, stat.rlisas, method="lesser")
-        gt = calculate_significance(stat.Is, stat.rlisas, method="greater")
-        fo = calculate_significance(stat.Is, stat.rlisas, method="folded")
-        corrs = (
-            pandas.DataFrame(
-                numpy.column_stack((ts, di, fo, lt, gt)),
-                columns=["two-sided", "directed", "folded", "lt", "gt"],
-            )
-            .corr()
-            .assign(repno=i)
-        )
-        all_correlations.append(corrs)
-    all_correlations = pandas.concat(all_correlations)
-"""
+    numpy.testing.assert_allclose(lesser + greater,
+                                  numpy.ones_like(lesser) + (1/(stat.permutations + 1)),
+        err_msg = "greater p-value should be complement of lesser"
+    )
+    assert (directed <= two_sided).all(), "directed is bigger than two_sided and should not be"
+    one_or_the_other = (directed == lesser) | (directed == greater)
+    assert one_or_the_other.all(), "some directed p-value is neither the greater nor lesser p-value"
+    assert numpy.abs(two_sided - folded).mean() < numpy.abs(lesser - folded).mean(), (
+        "folded p-values should be more similar to the two-sided variant than the directed"
+    )
