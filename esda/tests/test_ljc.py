291fa9c2c006161b18c275d1f8c889496333857e
James Gaboardi <jgaboardi@gmail.com>
2025-06-25 17:40:29 -0400

    ruff test_ljc.py

--- esda/tests/test_ljc.py
+++ esda/tests/test_ljc.py
@@ -1,8 +1,8 @@
 # based off: https://github.com/pysal/esda/blob/master/esda/tests/test_join_counts.py
 import numpy as np
+import pytest
 from libpysal import graph
 from libpysal.weights.util import lat2W
-import pytest
 
 from esda.join_counts_local import Join_Counts_Local
 
@@ -10,12 +10,11 @@ parametrize_w = pytest.mark.parametrize(
     "w",
     [
         lat2W(4, 4),
-        graph.Graph.from_W(
-             lat2W(4, 4)
-        ),
+        graph.Graph.from_W(lat2W(4, 4)),
     ],
     ids=["W", "Graph"],
 )
+
 
 class TestJoinCountsLocal:
     """Unit test for Local Join Counts (univariate)"""
@@ -25,8 +24,7 @@ class TestJoinCountsLocal:
         self.y[0:8] = 0
 
     @parametrize_w
-    def test_Join_Counts_Locals(self, w):
-        """Test method"""
+    def test_defaults(self, w):
         np.random.seed(12345)
         ljc = Join_Counts_Local(connectivity=w).fit(self.y)
         assert np.array_equal(ljc.LJC, [0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 2, 2, 3, 3, 2])
