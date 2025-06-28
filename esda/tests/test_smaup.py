278c78c7c82b9cecf676c542378cd29d90069c76
James Gaboardi <jgaboardi@gmail.com>
2025-06-27 20:52:23 -0400

    ruff test_smaup.py

--- esda/tests/test_smaup.py
+++ esda/tests/test_smaup.py
@@ -1,7 +1,6 @@
-import unittest
-
 import libpysal
 import numpy as np
+import pytest
 from libpysal.common import ATOL, RTOL, pandas
 
 from ..moran import Moran
@@ -10,8 +9,8 @@ from ..smaup import Smaup
 PANDAS_EXTINCT = pandas is None
 
 
-class Smaup_Tester(unittest.TestCase):
-    def setUp(self):
+class TestSmaup:
+    def setup_method(self):
         self.w = libpysal.io.open(libpysal.examples.get_path("stl.gal")).read()
         f = libpysal.io.open(libpysal.examples.get_path("stl_hom.txt"))
         self.y = np.array(f.by_col["HR8893"])
@@ -19,16 +18,16 @@ class Smaup_Tester(unittest.TestCase):
         self.n = len(self.y)
         self.k = int(self.n / 2)
 
-    def test_smaup(self):
+    def test_defaults(self):
         sm = Smaup(self.n, self.k, self.rho)
-        self.assertAlmostEqual(sm.n, 78)
-        self.assertAlmostEqual(sm.k, 39)
-        self.assertAlmostEqual(sm.rho, 0.24365582621771695)
+        assert sm.n == 78
+        assert sm.k == 39
+        assert pytest.approx(sm.rho) == 0.24365582621771695
         np.testing.assert_allclose(sm.smaup, 0.15221341690376405, rtol=RTOL, atol=ATOL)
-        self.assertAlmostEqual(sm.critical_01, 0.38970613333333337)
-        self.assertAlmostEqual(sm.critical_05, 0.3557221333333333)
-        self.assertAlmostEqual(sm.critical_1, 0.3157950666666666)
-        self.assertEqual(sm.summary, "Pseudo p-value > 0.10 (H0 is not rejected)")
+        assert pytest.approx(sm.critical_01) == 0.38970613333333337
+        assert pytest.approx(sm.critical_05) == 0.3557221333333333
+        assert pytest.approx(sm.critical_1) == 0.3157950666666666
+        assert sm.summary == "Pseudo p-value > 0.10 (H0 is not rejected)"
 
     def test_sids(self):
         w = libpysal.io.open(libpysal.examples.get_path("sids2.gal")).read()
@@ -39,12 +38,12 @@ class Smaup_Tester(unittest.TestCase):
         k = int(n / 2)
         sm = Smaup(n, k, rho)
         np.testing.assert_allclose(sm.smaup, 0.15176796553181948, rtol=RTOL, atol=ATOL)
-        self.assertAlmostEqual(sm.critical_01, 0.23404000000000003)
-        self.assertAlmostEqual(sm.critical_05, 0.21088)
-        self.assertAlmostEqual(sm.critical_1, 0.18239)
-        self.assertEqual(sm.summary, "Pseudo p-value > 0.10 (H0 is not rejected)")
+        assert pytest.approx(sm.critical_01) == 0.23404000000000003
+        assert pytest.approx(sm.critical_05) == 0.21088
+        assert pytest.approx(sm.critical_1) == 0.18239
+        assert sm.summary == "Pseudo p-value > 0.10 (H0 is not rejected)"
 
-    @unittest.skipIf(PANDAS_EXTINCT, "missing pandas")
+    @pytest.mark.skipif(PANDAS_EXTINCT, reason="missing pandas")
     def test_by_col(self):
         from libpysal.io import geotable as pdio
 
@@ -56,7 +55,7 @@ class Smaup_Tester(unittest.TestCase):
         rho = np.unique(mi.SIDR74_moran.values).item()
         sm = Smaup(w.n, k, rho)
         np.testing.assert_allclose(sm.smaup, 0.15176796553181948, atol=ATOL, rtol=RTOL)
-        self.assertAlmostEqual(sm.critical_01, 0.23404000000000003)
-        self.assertAlmostEqual(sm.critical_05, 0.21088)
-        self.assertAlmostEqual(sm.critical_1, 0.18239)
-        self.assertEqual(sm.summary, "Pseudo p-value > 0.10 (H0 is not rejected)")
+        assert pytest.approx(sm.critical_01) == 0.23404000000000003
+        assert pytest.approx(sm.critical_05) == 0.21088
+        assert pytest.approx(sm.critical_1) == 0.18239
+        assert sm.summary == "Pseudo p-value > 0.10 (H0 is not rejected)"
